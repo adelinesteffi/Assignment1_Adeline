3addbcbd962d26f5b78e04051b9054a9c26ff0d6
adelinesteffi <adelinesteffi1155@gmail.com>
2023-11-08 10:14:35 -0500

    cal-3

--- app/src/main/java/com/example/assignment1_adeline/Calculator.java
+++ app/src/main/java/com/example/assignment1_adeline/Calculator.java
@@ -1,35 +1,62 @@
 package com.example.assignment1_adeline;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Calculator extends AppCompatActivity implements View.OnClickListener {
-
-    EditText CalculateString;
-    Button One;
-    Button two;
-    Button Three;
-    Button Four;
-    Button Five;
-    Button Six;
-    Button Seven;
-
-    Button Eight, Nine,Zero;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
+public class Calculator extends AppCompatActivity  {
+    boolean validated = false;
+    public  boolean isValidInput(String input) {
+        Log.d("testing","sting ip in validate "+input);
+        Log.d("testing","in validate");
+        // Define a regular expression pattern to match one-digit numbers and valid operators
+       // String pattern = "^[0-9]([+\\-*/=][0-9])*[0-9]$"; // Matches one digit (0-9) followed by one of the operators +, -, *, /, =
+        String pattern = "^[0-9]([+\\-*/=][0-9])*$";
+        // Check if the input matches the pattern
+        return input.matches(pattern);
     }
+    public int performCal(String input)
+    {
+        Log.d("testing","sting ip in perform cal "+input);
+        int result =0;
+        char[] charArray = input.toCharArray();
+        int firstOperand , secondOperand;
+        char operator;
+        firstOperand =Integer.parseInt(String.valueOf(charArray[0]));
+        Log.d("testing","first op"+firstOperand);
+        Log.d("testing","charArray.length "+charArray.length);
 
-    @Override
-    public void onClick(View view) {
 
+        for(int i=0; i<charArray.length-1;i+=2)
+        {
+            secondOperand =Integer.parseInt(String.valueOf(charArray[i+2]));
+            operator = charArray[i+1];
+            Log.d("testing","i in loop "+i);
+            Log.d("testing","first op"+firstOperand);
+            Log.d("testing","2nd op"+secondOperand);
+            Log.d("testing","op"+operator);
+
+
+            switch (operator){
+                case '+': result= firstOperand+secondOperand; break;
+                case '-': result= firstOperand-secondOperand; break;
+                case '*': result= firstOperand*secondOperand; break;
+                case '/':
+                    if (secondOperand != 0) {
+                    result = firstOperand / secondOperand;
+                }
+                    break;
+            }
+            Log.d("testing","result after switch "+result);
+            firstOperand=result;
+            Log.d("testing","first op after switch"+firstOperand);
+            Log.d("testing","i after switch "+i);
+        }
+        Log.d("testing","result returned "+result);
+        return result;
     }
 }
--- app/src/main/java/com/example/assignment1_adeline/MainActivity.java
+++ app/src/main/java/com/example/assignment1_adeline/MainActivity.java
@@ -3,8 +3,108 @@ package com.example.assignment1_adeline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Calculator calObj= new Calculator();
 
-    
+    EditText CalculateString;
+    Button One;
+    Button Two;
+    Button Three;
+    Button Four;
+    Button Five;
+    Button Six;
+    Button Seven;
+
+    Button Eight, Nine,Zero, Multiply, Divide, Add, Subtract, EqualTo, Reset;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CalculateString = findViewById(R.id.display);
+        One = findViewById(R.id.OneButton);
+        Two = findViewById(R.id.TwoButton);
+        Three = findViewById(R.id.ThreeButton);
+        Four = findViewById(R.id.FourButton);
+        Five = findViewById(R.id.FiveButton);
+        Six = findViewById(R.id.SixButton);
+        Seven = findViewById(R.id.SevenButton);
+        Eight = findViewById(R.id.EightButton);
+        Nine = findViewById(R.id.NineButton);
+        Zero = findViewById(R.id.ZeroButton);
+        Add = findViewById(R.id.PlusButton);
+        Subtract = findViewById(R.id.MinusButton);
+        Divide = findViewById(R.id.DivisionButton);
+        Multiply = findViewById(R.id.MultiplicationButton);
+        EqualTo = findViewById(R.id.EqualToButton);
+        Reset = findViewById(R.id.ResetButton);
+        One.setOnClickListener(this);
+        Two.setOnClickListener(this);
+        Three.setOnClickListener(this);
+        Four.setOnClickListener(this);
+        Five.setOnClickListener(this);
+        Six.setOnClickListener(this);
+        Seven.setOnClickListener(this);
+        Eight.setOnClickListener(this);
+        Nine.setOnClickListener(this);
+        Zero.setOnClickListener(this);
+        Subtract.setOnClickListener(this);
+        Multiply.setOnClickListener(this);
+        Add.setOnClickListener(this);
+        Divide.setOnClickListener(this);
+        EqualTo.setOnClickListener(this);
+        Reset.setOnClickListener(this);
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Get the text of the button that was clicked
+        Button clickedButton = (Button) view;
+        String buttonText = clickedButton.getText().toString();
+
+        // Get the current text in the EditText
+        String currentText = CalculateString.getText().toString();
+if(clickedButton != EqualTo && clickedButton != Reset) {
+    // Append the button's text to the current text
+    String updatedText = currentText + buttonText;
+    Log.d("testing", "in onclick");
+    // Set the updated text in the EditText
+    CalculateString.setText(updatedText);
+}
+        if(clickedButton == EqualTo)
+        {
+            Log.d("testing","equal to pressed");
+           if( calObj.isValidInput(currentText))
+            {
+                Log.d("testing","in if");
+                Log.d("testing","sting ip in if currentText  "+currentText);
+              int result=  calObj.performCal(currentText);
+                Log.d("testing","result in main "+result);
+                String resultString = String.valueOf(result);
+                Log.d("testing","resultString in main "+resultString);
+                CalculateString.setText(currentText+"="+resultString);
+            }
+           else {
+               // Show a toast message for invalid input
+               Log.d("testing","in else");
+               Toast.makeText(this, "Enter one-digit number or valid operator", Toast.LENGTH_SHORT).show();
+           }
+
+            }
+        if (clickedButton == Reset) {
+            Log.d("testing", "in reset btn clear");
+            CalculateString.setText("");
+        }
+
+    }
 }
